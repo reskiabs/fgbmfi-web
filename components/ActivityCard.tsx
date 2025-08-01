946bd5b536b6d21f27a3bb836cad9b3b19f7a090
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2025-08-01 10:31:39 +0700

    feat: update padding time in activity card

--- components/ActivityCard.tsx
+++ components/ActivityCard.tsx
@@ -14,9 +14,9 @@ const ActivityCard = ({ src, title, date, href }: ActivityCardProps) => {
       <div className="relative h-[170px] w-[340px] rounded-[15px] overflow-hidden md:w-[450px] md:h-[272px] lg:w-[550px] lg:h-[309px] md:rounded-[20px]">
         <Image src={src} alt={title} fill className="object-center" />
       </div>
-      <div className="mt-2.5 md:mt-5 space-y-0.5 md:space-y-2.5">
+      <div className="mt-2.5 md:mt-5">
         <h2 className="font-bold text-[15px] md:text-xl">{title}</h2>
-        <p className="font-medium text-[10px] text-gray-400 md:text-[15px]">
+        <p className="font-medium text-[10px] text-gray-400 md:text-[15px] md:mt-0.5">
           {dayjs(date).format("DD MMMM YYYY")}
         </p>
       </div>
