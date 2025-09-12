7a983fe9177ab0451708deb8323d118a457e0a25
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2025-09-12 08:27:04 +0700

    style: update image carouselon mobile view

--- components/ImageCarousel.tsx
+++ components/ImageCarousel.tsx
@@ -40,7 +40,7 @@ const ImageCarousel = ({ images }: { images: string[] }) => {
 
   return (
     <div className="relative group">
-      <div className="relative w-[340px] h-[191px] rounded-[15px] overflow-hidden md:w-[920px] md:h-[517px] lg:w-[1140px] lg:h-[641px] md:rounded-[20px] hover:cursor-pointer">
+      <div className="relative w-full min-w-[340px] h-[191px] rounded-[15px] overflow-hidden md:w-[920px] md:h-[517px] lg:w-[1140px] lg:h-[641px] md:rounded-[20px] hover:cursor-pointer">
         {images.map((src, index) => (
           <Image
             key={index}
